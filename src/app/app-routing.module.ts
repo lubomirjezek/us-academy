a25a6345989ce624109f6795c153e2a82df3b309
Lubomir Jezek <lj@mcrai.eu>
2018-12-16 18:00:47 +0100

    lay down base fordel structure and add routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './pages/homepage/homepage.module#HomepageModule'
+  }, {
+    path: 'gdpr',
+    loadChildren: './pages/gdpr/gdpr.module#GDPRModule'
+  }, {
+    path: 'detail',
+    loadChildren: './pages/detail/detail.module#DetailModule'
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { enableTracing: false }),
+  ],
+  exports: [
+    RouterModule,
+  ]
+})
+export class AppRoutingModule { }

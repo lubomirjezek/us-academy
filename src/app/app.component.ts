166c826050759021cc7679384ab5fd22fae9b89a
Lubomir Jezek <lj@mcrai.eu>
2018-12-23 16:13:11 +0100

    scroll to top when route changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,43 @@
-import { AfterViewInit, Component, HostBinding } from '@angular/core';
-import { timer } from 'rxjs';
+import { AfterViewInit, Component, HostBinding, Inject, OnDestroy, OnInit } from '@angular/core';
+import { Subject, timer } from 'rxjs';
+import { NavigationCancel, NavigationEnd, Router, RouterEvent } from '@angular/router';
+import { takeUntil } from 'rxjs/operators';
+
+import { WINDOW } from './app.config';
 
 @Component({
   selector: 'usacademy-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
+  destroy: Subject<null> = new Subject<null>();
 
   @HostBinding('class.intro') showInto = true;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    @Inject(WINDOW) private window: Window
+  ) { }
+
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        takeUntil(this.destroy)
+      )
+      .subscribe((event: RouterEvent) => {
+        switch (true) {
+          case event instanceof NavigationEnd:
+          case event instanceof NavigationCancel:
+            this.window.scrollTo(0, 0);
+            break;
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy.next();
+  }
 
   ngAfterViewInit() {
     timer(800)
